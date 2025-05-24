04d03ed0fc41fad245b8ec6341b5f0846a334f51
chanlinna <chanlinnalin@gmail.com>
2025-05-24 22:22:04 +0700

    server.js

--- W4-Practice/StartCode/EX-2/server.js
+++ W4-Practice/StartCode/EX-2/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
-import { articles } from './models/data';
+import articlesRoutes from './routes/articlesRoutes.js';
+import journalistsRoutes from './routes/journalistsRoutes.js';
+import categoriesRoutes from './routes/categoriesRoutes.js';
+import logger from './middleware/logger.js';
 
 const app = express();
 
 const PORT = 3000;
+app.use(express.json());
+app.use(logger);
 
 // app.get('/', (req, res) => {
 //     res.json(articles)
 // });
+
+app.use('/articles', articlesRoutes);
+app.use('/journalists', journalistsRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
