5344b776fadd23dc5543dab7f0f65c0afe2e4b75
chanlinna <chanlinnalin@gmail.com>
2025-06-15 16:26:34 +0700

    ex2

---
+++ W6-SQL_Integration/Start_Code/front/src/components/ArticlePage.jsx
@@ -0,0 +1,59 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getArticleById } from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+export default function ArticlePage() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [article, setArticle] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchArticle();
+  }, []);
+
+
+  const fetchArticle = async () => {
+    try {
+      setLoading(true);
+
+      const found = getArticleById(id);
+      if (found) {
+        setArticle(found);
+        setError("");
+      } else {
+        setArticle(null);
+        setError("Article not found.");
+      }
+    } catch (err) {
+      setError("Failed to fetch article.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <div>Loading article...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!article) return <div>No article found.</div>;
+
+  return (
+    <div>
+      <h2>{article.title}</h2>
+      <p>{article.content}</p>
+      <div>
+        <strong>Journalist:</strong> {" "}
+        <span
+          style={{ color: "blue", cursor: "pointer" }}
+          onClick={() => navigate(`/journalists/${article.journalistId}/articles`)}>
+          {article.journalistName}
+        </span>
+      </div>
+      <div>
+        <strong>Category:</strong> {article.category}
+      </div>
+    </div>
+  );
+}
